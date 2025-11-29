067e1995db8180ca29d5e64524909ea701788548
DIDDEKUNTA HARATHI <diddekuntaharathi@gmail.com>
2025-11-29 20:26:39 +0530

    Add files via upload

---
+++ area of square.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int length,breadth,area;
+	printf("enter length and breadth values");
+	scanf("%d%d",&length,&breadth);
+	area=length*breadth;
+	printf("the area of rectangle is %d",area);
+	return 0;
+}
