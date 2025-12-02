508f1cbf63306e9432c8bf20a32ad9673592278d
DIDDEKUNTA HARATHI <diddekuntaharathi@gmail.com>
2025-12-02 09:26:10 +0530

    Add files via upload

---
+++ ARTHEMETIC OPERATIONS.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int a,b,c,d,e,f,g;
+	printf("enter a and b values");
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	d=a-b;
+	e=a*b;
+	f=a/b;
+	g=a%b;
+	printf("%d\n%d\n%d\n%d\n%d",c,d,e,f,g);
+	return 0;
+}
